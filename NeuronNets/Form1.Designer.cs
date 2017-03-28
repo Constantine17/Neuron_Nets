73be33a37657e95a8a3cfb68a487b79591dd0fdd
Constantine17 <constantine.v.cherkashyn@gmail.com>
2017-03-28 12:13:08 +0300

    Neuron Nets v0.9

---
+++ NeuronNets/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace NeuronNets
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Clear = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.yotv = new System.Windows.Forms.TextBox();
+            this.plus = new System.Windows.Forms.Button();
+            this.minus = new System.Windows.Forms.Button();
+            this.sum = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(249, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(764, 627);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Перцептроны";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.pictureBox1.Location = new System.Drawing.Point(45, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(150, 150);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // Clear
+            // 
+            this.Clear.Location = new System.Drawing.Point(124, 184);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(108, 23);
+            this.Clear.TabIndex = 6;
+            this.Clear.Text = "Очистить";
+            this.Clear.UseVisualStyleBackColor = true;
+            this.Clear.Click += new System.EventHandler(this.Clear_Click);
+            this.Clear.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Clear_KeyDown);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 184);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(102, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Принять";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // yotv
+            // 
+            this.yotv.Location = new System.Drawing.Point(107, 407);
+            this.yotv.Name = "yotv";
+            this.yotv.Size = new System.Drawing.Size(56, 20);
+            this.yotv.TabIndex = 9;
+            // 
+            // plus
+            // 
+            this.plus.Location = new System.Drawing.Point(12, 315);
+            this.plus.Name = "plus";
+            this.plus.Size = new System.Drawing.Size(102, 23);
+            this.plus.TabIndex = 10;
+            this.plus.Text = "A";
+            this.plus.UseVisualStyleBackColor = true;
+            this.plus.Click += new System.EventHandler(this.plus_Click);
+            // 
+            // minus
+            // 
+            this.minus.Location = new System.Drawing.Point(124, 315);
+            this.minus.Name = "minus";
+            this.minus.Size = new System.Drawing.Size(104, 23);
+            this.minus.TabIndex = 11;
+            this.minus.Text = "Б";
+            this.minus.UseVisualStyleBackColor = true;
+            this.minus.Click += new System.EventHandler(this.minus_Click);
+            // 
+            // sum
+            // 
+            this.sum.Location = new System.Drawing.Point(120, 256);
+            this.sum.Name = "sum";
+            this.sum.Size = new System.Drawing.Size(104, 20);
+            this.sum.TabIndex = 12;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 410);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 13);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Результат сети:";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(21, 477);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(216, 17);
+            this.checkBox1.TabIndex = 14;
+            this.checkBox1.Text = "Показать конвертацию изображения";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(21, 500);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(121, 17);
+            this.checkBox2.TabIndex = 15;
+            this.checkBox2.Text = "Показывать цвета";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(70, 259);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Сумма:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 439);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(231, 23);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "Определить символ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(1027, 651);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.sum);
+            this.Controls.Add(this.minus);
+            this.Controls.Add(this.plus);
+            this.Controls.Add(this.yotv);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Нейроная сеть";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button Clear;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox yotv;
+        private System.Windows.Forms.Button plus;
+        private System.Windows.Forms.Button minus;
+        private System.Windows.Forms.TextBox sum;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ NeuronNets/Form1.cs
@@ -0,0 +1,311 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NeuronNets
+{
+    public partial class Form1 : Form
+    {
+        //объявление общих элементов
+        TextBox[,] tb = new TextBox[30,30]; //объявление текстбоксов
+        Bitmap b;
+        Graphics graf;
+        bool prov = false;
+        Color fist;
+        int[,] vX=new int[30,30];
+        double[,] W = new double[30, 30];
+        int Y = 0;
+        
+        double T = 0; // задаем Т
+        double alpha = 0.5; //задаем скорость обучения
+
+        double S = 0;
+
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            for (int j = 0; j < 30; j++)
+            {
+                for (int i = 0; i < 30; i++)
+                {
+                    W[i,j] = 0.4; // первое значение весовой функции
+                    
+                }
+            }
+
+
+            int x = 7, y =20;
+            //TextBox[] tb = new TextBox[20];
+            for (int j=0; j<30;j++ )
+            {
+                for (int i = 0; i < 30; i++)
+                {
+                    tb[i, j] = new TextBox() //свойства текстбоксов
+                    {
+                        Location = new Point(x, y),
+                        Text = "0.5",
+                        Size = new Size(24, 20)
+                    };
+                    //бла-бла-бла
+                    this.groupBox1.Controls.Add(tb[i, j]);
+                    x += 25;
+                }
+                x = 7; y += 20;
+            }
+            Bitmap savedBit = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.DrawToBitmap(savedBit, pictureBox1.ClientRectangle);
+            fist = savedBit.GetPixel(1,1);//цвет фона изображения
+            
+        }
+        
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //int u=1;
+            for (int i = 0; i < 4; i++)
+            {
+
+                //Graphics g = Graphics.FromHwnd(panel1.Handle);
+            }
+           // Graphics graf = pictureBox1.CreateGraphics();
+
+            //pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+           // Image bmp = pictureBox1.Image;
+            //graf = Graphics.FromImage(bmp);
+           // g.DrawLine(new Pen(Brushes.Black, 2f), 0, 20, 100, 20);
+           // pictureBox1.Image.Save("image.png", System.Drawing.Imaging.ImageFormat.Png);
+
+
+            Bitmap savedBit = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.DrawToBitmap(savedBit, pictureBox1.ClientRectangle);
+
+           
+            //savedBit = (Bitmap)pictureBox1.Image;
+
+          //  Graphics graf = Graphics.FromImage(savedBit);
+            
+            SaveFileDialog k = new SaveFileDialog();
+            if (k.ShowDialog() == DialogResult.OK)
+
+                savedBit.Save(k.FileName);
+
+        }
+        
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void textBox11_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        bool DoDraw = false;
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            DoDraw = true;
+        }
+
+
+
+
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            DoDraw = true; 
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            DoDraw = false;
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            //if (DoDraw)//рисование
+            {
+
+                if (DoDraw)
+                {
+                    prov = false; b = new Bitmap(pictureBox1.Width, pictureBox1.Height); //преобразования рисунка в изображение
+                    graf = Graphics.FromImage(b);
+                    Graphics grafpro = Graphics.FromImage(b);
+                    SolidBrush redBrush = new SolidBrush(Color.Black);//кисть рисования
+                    graf.FillEllipse(redBrush, e.X, e.Y, 10, 10);
+
+                    pictureBox1.Image = b;
+                }
+
+            }
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            
+            pictureBox1.Invalidate();
+            if(!prov) graf.Clear(Color.White); //очистка изображения
+            prov = true;
+            yotv.Text = "";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Bitmap savedBit = new Bitmap(pictureBox1.Width, pictureBox1.Height);//преобразования рисунка в изображение
+            pictureBox1.DrawToBitmap(savedBit, pictureBox1.ClientRectangle);
+            
+           // Color pixelColor = savedBit.GetPixel(50, 50);
+           // pictureBox1.Invalidate();
+            //graf.Clear(pixelColor);
+
+            for (int y=0, j=2; y < 30; y++)
+            {
+                for (int x =0, i=2; x < 30; x++)
+                {
+
+                    if (savedBit.GetPixel(i, j) == fist) vX[x, y] = 0;else vX[x, y] = 1;//ввод данных
+                    if (checkBox1.Checked == true)//проверка чекбокса переобразование 
+                    { tb[x, y].Text = Convert.ToString(vX[x, y]); tb[x, y].BackColor = Color.White; }//обнуление цвета
+                    i = i + 5;
+                }
+                j = j + 5;
+            }
+
+            for (int j = 0; j < 30; j++)
+            {
+                for (int i = 0; i < 30; i++)
+                {
+                    S = S + (vX[i, j] * W[i, j] - T);
+                }
+            }
+            sum.Text = Convert.ToString(S);
+            S = 0;
+            yotv.Text = "";//обнуление вывода
+             
+        }
+
+
+        private void plus_Click(object sender, EventArgs e)
+        {
+            S = 0;
+
+            for (int j = 0; j < 30; j++)//суммирование
+            {
+                for (int i = 0; i < 30; i++)
+                {
+                    S = S + (vX[i, j] * W[i, j] - T);
+                }
+            }
+
+            if (S >= 0) Y = 1; else Y = -1;//вывод сети
+
+            for (int j = 0; j < 30; j++)
+            {
+                for (int i = 0; i < 30; i++)
+                {
+                    W[i, j] = W[i, j] - alpha * vX[i, j] * (Y - 1);//обучение сети
+                }
+            }
+            T = T + alpha * (Y - 1);
+
+            for (int j = 0; j < 30; j++)// вывод значения персептронов
+            {
+                for (int i = 0; i < 30; i++)
+                {
+                    tb[i, j].Text = Convert.ToString(W[i, j]);
+                    if (checkBox2.Checked == true) //проверка чек бокса на цвет
+                        if (W[i, j] > 0.5)
+                            tb[i, j].BackColor = Color.Green;
+                        else if (W[i, j] < 0.3) tb[i, j].BackColor = Color.Red; else tb[i, j].BackColor = Color.White; ;
+                }
+            }
+
+            if (Y == 1) yotv.Text = "A"; else yotv.Text = "Б";//вывод значение сети в текстбокс
+            S = 0;
+        }
+
+        private void minus_Click(object sender, EventArgs e)
+        {
+            S = 0;
+            for (int j = 0; j < 30; j++)//суммирование
+            {
+                for (int i = 0; i < 30; i++)
+                {
+                    S = S + (vX[i, j] * W[i, j] - T);
+                }
+            }
+
+            if (S >= 0) Y = 1; else Y = -1;//вывод сети
+
+            for (int j = 0; j < 30; j++)
+            {
+                for (int i = 0; i < 30; i++)
+                {
+                    W[i, j] = W[i, j] - alpha * vX[i, j] * (Y + 1);//обучение сети
+                }
+            }
+            T = T + alpha * (Y + 1);
+
+            for (int j = 0; j < 30; j++)// вывод значения персептронов
+            {
+                for (int i = 0; i < 30; i++)
+                { 
+                    tb[i, j].Text = Convert.ToString(W[i, j]);
+                    if (checkBox2.Checked == true)//проверка чек бокса на цвет
+                        if (W[i, j] > 0.5)
+                            tb[i, j].BackColor = Color.Green;
+                        else if (W[i, j] < 0.3) tb[i, j].BackColor = Color.Red; else tb[i, j].BackColor = Color.White; ; 
+                }
+            }
+            if (Y == 1) yotv.Text = "A"; else yotv.Text = "Б";//вывод значение сети в текстбокс
+            S = 0;
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            for (int j = 0; j < 30; j++)//суммирование
+            {
+                for (int i = 0; i < 30; i++)
+                {
+                    S = S + (vX[i, j] * W[i, j] - T);
+                }
+            }
+
+            if (S >= 0) Y = 1; else Y = -1;//вывод сети
+            if (Y == 1) yotv.Text = "A"; else yotv.Text = "Б";//вывод значение сети в текстбокс
+            S = 0;
+        }
+
+        private void Clear_KeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+        
+
+    }
+}
